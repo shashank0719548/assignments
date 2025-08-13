f57f5339a35d01ec4068ca7132025139d9a55d1f
shashank.P <shashank_ponnuru@srmap.edu.in>
2025-08-13 22:56:17 +0530

    lab-1

--- Daa-lab1/linearsearch.c
+++ Daa-lab1/linearsearch.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 
-int linear_search(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++)
-        if (arr[i] == key)
-            return i;
-    return -1;
-}
-
-int binary_search(int arr[], int n, int key) {
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (arr[mid] == key)
-            return mid;
-        else if (arr[mid] < key)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return -1;
-}
-
 int main() {
-    int n, key, result;
+    int n, key, i, found = 0;
     printf("Enter size of array: ");
     scanf("%d", &n);
     int arr[n];
     printf("Enter %d elements: ", n);
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
     printf("Enter key to search: ");
     scanf("%d", &key);
-    result = linear_search(arr, n, key);
-    if (result != -1)
-        printf("Linear Search: Found at index %d\n", result);
-    else
-        printf("Linear Search: Not found\n");
-    result = binary_search(arr, n, key);
-    if (result != -1)
-        printf("Binary Search: Found at index %d\n", result);
-    else
-        printf("Binary Search: Not found\n");
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+        printf("Not found\n");
     return 0;
 }
